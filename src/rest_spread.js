95240016cb6a68227781e3e1596cfe841ffa19b8
fabricol <fabricioctnmiranda@gmail.com>
2024-05-08 08:46:39 -0300

    operador rest/spread

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -7,7 +7,7 @@
 //     return soma;
 // }
 
-console.log(somar(10,20,30))
+// console.log(somar(10,20,30))
 
 // Criação do operador rest
 function somarComRest(...numeros) {
@@ -17,3 +17,33 @@ function somarComRest(...numeros) {
     }, 0)
     return soma;
 }
+
+
+// spread, espalhar os itens de um array
+const numeros3 = [3,6,9,12]
+console.log(...numeros3)
+
+console.log(numeros3)
+
+const times = ['america','corinthians']
+const times2 = ['palmeiras','sp']
+
+const timeFut = [...times,...times2]
+
+console.log(timeFut)
+
+
+// Como copiar as propriedades de outro objeto com o rest?
+
+const carroJulia = {
+    modelo: 'gol',
+    marca: 'Vw',
+    motor: 1.6
+}
+// O carro da Ana só muda o motor
+
+const carroAna = {
+    ...carroJulia,
+    motor: 1.8
+}
+
